a2ba6e3178b6d529b7bee1d17e9a66bd663d2df8
Pedro Galvão <taquiles@gmail.com>
2021-01-06 13:00:46 +0000

    section 12.142

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Accordion from './components/Accordion';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const items=[
+  {
+    title:'What is reat',
+    content: "React is a froint end javascript frontend"
+  },
+  {
+    title:'Why React',
+    content: "React is a favorite among developers and, of course, is trendy"
+  },
+  {
+    title:'How do you use React',
+    content: "By creacting components"
+  }
+];
+
+const App = () => {
+  return(
+    <Accordion items={items}/>
   );
-}
+};
 
 export default App;
+
