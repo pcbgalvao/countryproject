87097204e537985ce59e135648db181425053930
Pedro Galvão <taquiles@gmail.com>
2021-01-12 16:17:21 +0000

    section 12.170

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,46 +1,99 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
-import axios from 'axios';
+import axios from "axios";
 
 const Search = () => {
-  const [term, setTerm] = useState('programming');
+  const [term, setTerm] = useState("programming");
   const [results, setResults] = useState([]);
+  const [debouncedTerm, setdebouncedTerm] = useState(term);
 
   const onInputChange = (event) => {
     setTerm(event.target.value);
-  }
+  };
 
-  //console.log ("I'm a runners, and always run");
-
-  useEffect (() => {
+  useEffect(() => {
     const search = async () => {
-      const {data} = await axios.get('https://en.wikipedia.org/w/api.php',{
+      const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
         params: {
-          actions: 'query',
+          action: 'query',
           list: 'search',
           origin: '*',
           format: 'json',
-          srsearch: term
-        }      
+          srsearch: debouncedTerm,
+        },
+      });
+ 
+      setResults(data.query.search);
+    };
+    if (debouncedTerm) {
+      search();
+    }
+  }, [debouncedTerm]);
+
+
+  useEffect(() => {
+    const search = async () => {
+      const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
+        params: {
+          action: "query",
+          list: "search",
+          origin: "*",
+          format: "json",
+          srsearch: term,
+        },
       });
       setResults(data.query.search);
     };
 
-    search('');
-  }, [term])
+    if (term && !results.length) {
+      search();
+    } else {
+      const timeoutId = setTimeout(() => {
+        if (term) {
+          search();
+        }
+      }, 1000);
+
+      return () => {
+        clearTimeout(timeoutId);
+      };
+    }
+  }, [term, results.length]);
+
+  const renderedResults = results.map((result) => {
+    return (
+      <div className="item" key={result.pageid}>
+        <div className="right floated content">
+          <a
+            className="ui button"
+            href={`https://en.wikipedia.org?curid=${result.pageid}`}
+          >
+            Go
+          </a>
+        </div>
+        <div className="content">
+          <div className="header">{result.title}</div>
+          <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
+        </div>
+      </div>
+    );
+  });
 
   return (
-    <div className="ui form">
-      <div className="field">
-        <label>Enter Search Term</label>
-        <input 
-          className="input" 
-          value={term}
-          onChange={onInputChange}
-        />
+    <div>
+      <div className="ui form">
+        <div className="field">
+          <label>Enter Search Term</label>
+          <input
+            className="input"
+            value={term}
+            onChange={(e) => setTerm(e.target.value)}
+          />
+        </div>
       </div>
+      <div className="ui celled list">{renderedResults}</div>
     </div>
-  )
-}
+  );
+};
 
 export default Search;
