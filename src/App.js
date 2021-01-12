41de8b15de11424738d675d012d7086d152bb831
Pedro Galvão <taquiles@gmail.com>
2021-01-12 23:22:27 +0000

    section 12.178

--- src/App.js
+++ src/App.js
@@ -1,29 +1,54 @@
-import React from 'react';
-import Accordion from './components/Accordion';
-import Search from './components/Search';
+import React, { useState } from "react";
+import Accordion from "./components/Accordion";
+import Search from "./components/Search";
+import Dropdown from "./components/Dropdown";
 
-const items=[
+const items = [
   {
-    title:'What is react',
-    content: "React is a froint end javascript frontend"
+    title: "What is react",
+    content: "React is a froint end javascript frontend",
   },
   {
-    title:'Why React',
-    content: "React is a favorite among developers and, of course, is trendy"
+    title: "Why React",
+    content: "React is a favorite among developers and, of course, is trendy",
   },
   {
-    title:'How do you use React',
-    content: "By creacting components"
-  }
+    title: "How do you use React",
+    content: "By creacting components",
+  },
+];
+
+const options = [
+  {
+    label: "The Color Red",
+    value: "red",
+  },
+  {
+    label: "The Color Blue",
+    value: "blue",
+  },
+  {
+    label: "The Color Green",
+    value: "green",
+  },
+  {
+    label: "A Shade of Blue",
+    value: "shade blue",
+  },
 ];
 
 const App = () => {
-  return(
+  const [selected, setSelected] = useState(options[0]);
+
+  return (
     <div>
-      <Search />
+      <Dropdown
+        selected={selected}
+        onSelectedChange={setSelected}
+        options={options}
+      />
     </div>
   );
 };
 
 export default App;
-
