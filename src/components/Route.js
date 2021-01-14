99cf2dee823070aa8bf202f175caf215a975d9cc
Pedro Galvão <taquiles@gmail.com>
2021-01-14 16:33:25 +0000

    section 13.207

--- src/components/Route.js
+++ src/components/Route.js
@@ -1,5 +1,25 @@
+import { useEffect, useState } from "react";
+
 const Route = ({ path, children }) => {
-  return window.location.pathname === path ? children : null;
+  const [currentPath, setCurrentPath] = useState(window.location.pathname);
+  console.log("Inseide Route Component");
+  
+  useEffect(() => {
+    console.log("Inside useEffect onLocationChange");
+    const onLocationChange = () => {
+      console.log("Inside Route.onLocationChange");
+      setCurrentPath(window.location.pathname);
+    };
+
+    window.addEventListener("popstate", onLocationChange);
+
+    return () => {
+      console.log("Route.removeEventListener");
+      window.removeEventListener("popstate", onLocationChange);
+    };
+  }, []);
+
+  return currentPath === path ? children : null;
 };
 
 export default Route;
--- src/components/Translate.js
+++ src/components/Translate.js
@@ -3,7 +3,6 @@ import React, { useState, useEffect } from "react";
 import Dropdown from "./Dropdown";
 import Convert from "./Convert";
 
-
 const options = [
   {
     label: "Afrikaans",
